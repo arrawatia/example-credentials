11f90cfd94d9cc1108ac6153c3cac2327407c94a
Nathaniel Eliot <temujin9@t9productions.com>
2012-12-17 19:55:18 -0600

    Cleaning up knife-user-example.rb

--- knife-user-example.rb
+++ knife-user-example.rb
@@ -1,19 +1,20 @@
+Chef::Config.instance_eval do
+  #
+  # Please rename this file 'knife-user-{username}.rb' For example, my (mrflip's)
+  # file is 'knife-user-mrflip.rb'.
+  #
 
-#
-# Please rename this file 'knife-user-{username}.rb' For example, my (mrflip's)
-# file is 'knife-user-mrflip.rb'.
-#
+  #
+  # EC2 cloud keys
+  #
 
-#
-# EC2 cloud keys - if you use this (you should) remove the keys from knife-org.rb
-#
+  # knife[:aws_access_key_id]      = "XXX"
+  # knife[:aws_secret_access_key]  = "XXX"
 
-# Chef::Config.knife[:aws_access_key_id]      = "XXX"
-# Chef::Config.knife[:aws_secret_access_key]  = "XXX"
-
-#
-# Cookbook templating -- only
-#
-# Chef::Config[:cookbook_copyright] =  "Bob Dobbs Jr, fnord.com"
-# Chef::Config[:cookbook_email    ] =  "bob@fnord.com"
-# Chef::Config[:cookbook_license  ] =  "apachev2"
+  #
+  # Cookbook templating -- only
+  #
+  # Chef::Config[:cookbook_copyright] =  "Bob Dobbs Jr, fnord.com"
+  # Chef::Config[:cookbook_email    ] =  "bob@fnord.com"
+  # Chef::Config[:cookbook_license  ] =  "apachev2"
+end
